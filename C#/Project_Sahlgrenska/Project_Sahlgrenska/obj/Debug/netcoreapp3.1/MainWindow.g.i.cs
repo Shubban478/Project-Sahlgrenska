27a88ea0d59a3c82f6815894ab47357ec7d413c6
tutte2k <45877330+tutte2k@users.noreply.github.com>
2021-11-29 19:32:51 +0100

    rättat miss

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Project_Sahlgrenska {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Project_Sahlgrenska;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Project_Sahlgrenska;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
