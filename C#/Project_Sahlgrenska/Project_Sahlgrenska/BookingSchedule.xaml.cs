55f1bb9d20eaea166fd813cd8b98fa12ec62045d
tutte2k <45877330+tutte2k@users.noreply.github.com>
2021-11-29 19:28:10 +0100

    lagt in windows

---
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/BookingSchedule.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_Sahlgrenska
+{
+    /// <summary>
+    /// Interaction logic for BookingSchedule.xaml
+    /// </summary>
+    public partial class BookingSchedule : Window
+    {
+        public BookingSchedule()
+        {
+            InitializeComponent();
+        }
+    }
+}
