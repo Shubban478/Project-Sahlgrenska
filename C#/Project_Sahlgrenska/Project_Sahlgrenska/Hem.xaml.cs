9284f1849e5d910cb36eb56e65c62f2abcd1664b
tutte2k <45877330+tutte2k@users.noreply.github.com>
2021-11-30 11:56:05 +0100

    färdigställt kod så fönster öppnas
    nu är det bara köra

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/Hem.xaml.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/Hem.xaml.cs
@@ -28,5 +28,41 @@ namespace Project_Sahlgrenska
             this.Close();
             login.Show();
         }
+
+        private void newPatient_Click(object sender, RoutedEventArgs e)
+        {
+            NewPatient newPatient = new NewPatient();
+            newPatient.Show();
+        }
+
+        private void searchPatient_Click(object sender, RoutedEventArgs e)
+        {
+            SearchPatient searchPatient = new SearchPatient();
+            searchPatient.Show();
+        }
+
+        private void bookRoom_Click(object sender, RoutedEventArgs e)
+        {
+            BookRoom bookRoom = new BookRoom();
+            bookRoom.Show();
+        }
+
+        private void bookingSchedule_Click(object sender, RoutedEventArgs e)
+        {
+            BookingSchedule bookingSchedule = new BookingSchedule();
+            bookingSchedule.Show();
+        }
+
+        private void orderItems_Click(object sender, RoutedEventArgs e)
+        {
+            OrderItems orderItems = new OrderItems();
+            orderItems.Show();
+        }
+
+        private void diseaseList_Click(object sender, RoutedEventArgs e)
+        {
+            DiseaseList diseaseList = new DiseaseList();
+            diseaseList.Show();
+        }
     }
 }
