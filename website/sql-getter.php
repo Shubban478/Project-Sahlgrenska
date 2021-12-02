b705fb25dca5036a81ace0e7e8d5d4062595b3a5
zeapoz <zeapo@pm.me>
2021-12-02 11:18:13 +0100

    Removed sql-getter.js + added port for mysqli

--- website/sql-getter.php
+++ website/sql-getter.php
@@ -4,13 +4,14 @@ function query() {
     $username = "ulhpxhgnf5tkywq2";
     $password = "CE2AriOp5v9YqliNasMM";
     $dbname = "bjjyx4krtcspjgah0tay";
+    $port = "3306";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
     // Check connection
     if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
     }
     echo "Successfully connected to the database.<br><br>";
 
