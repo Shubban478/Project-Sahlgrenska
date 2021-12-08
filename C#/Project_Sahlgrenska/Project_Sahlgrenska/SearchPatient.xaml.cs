493847ebb14d0d15f8145db31851a8147f2ab86c
Jonathan Limén <jonathan.esbjornsson@gmail.com>
2021-12-08 10:49:03 +0100

    Cleaned up the code

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/SearchPatient.xaml.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/SearchPatient.xaml.cs
@@ -24,48 +24,15 @@ namespace Project_Sahlgrenska
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var PatientData = Bot.ReadOneLine("select * from bjjyx4krtcspjgah0tay.patients where ID LIKE '" + searchPatient.Text + "'");
 
-            patientId.Text = "";
-            patientGender.Text = "";
-            patientName.Text = "";
-            patientAdress.Text = "";
-            patientAdmitted.Text = "";
-            patientDischarged.Text = "";
-            patientBooking.Text = "";
-            patientReason.Text = "";
-            patientHistory.Text = "";
-
-            Hem.conn.Open();
-            string sql = "select * from bjjyx4krtcspjgah0tay.patients where id LIKE '" + searchPatient.Text + "'";
-            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql, Hem.conn);
-            MySql.Data.MySqlClient.MySqlDataReader rdr = cmd.ExecuteReader();
-
-            while (rdr.Read())
-            {
-                patientId.AppendText(rdr.GetString(0));
-                patientGender.AppendText(rdr.GetString(3));
-                patientName.AppendText(rdr.GetString(1));
-                patientAdress.AppendText(rdr.GetString(2));
-
-                if (rdr.IsDBNull(5)) { patientAdmitted.Text = "SAKNAS"; }
-                else { patientAdmitted.AppendText(rdr.GetString(4)); }
-
-                if (rdr.IsDBNull(5)) { patientDischarged.Text = "SAKNAS"; }
-                else { patientDischarged.AppendText(rdr.GetString(5)); }
-
-                if (rdr.IsDBNull(8)) { patientBooking.Text = "SAKNAS"; }
-                else { patientBooking.AppendText(rdr.GetString(8)); }
-
-                if (rdr.IsDBNull(6)) { patientReason.Text = "SAKNAS"; }
-                else { patientReason.AppendText(rdr.GetString(6)); ; }
-
-                if (rdr.IsDBNull(7)) { patientHistory.Text = "SAKNAS"; }
-                else { patientHistory.AppendText(rdr.GetString(7)); }
-            }
-            
-            rdr.Close();
-            Hem.conn.Close();
+            patientId.Text = PatientData[0];
+            //patientName.Text = PatientData[1];
+            //patientAdress.Text = PatientData[2];
+            //patientGender.Text = PatientData[3];
+            //patientAdmitted.Text = PatientData[4];
+            //patientDischarged.Text = PatientData[5];
+            patientHistory.Text = PatientData[6];
         }
-
     }
 }
