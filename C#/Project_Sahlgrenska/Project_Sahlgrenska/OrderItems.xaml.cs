d3ea4d636b1b75bd65fd9acdd5914cbca9182050
Sebastian Lundgren <lundgren_sebastian@hotmail.com>
2021-12-08 22:18:59 +0100

    Beställningar utkast

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/OrderItems.xaml.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/OrderItems.xaml.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Project_Sahlgrenska
 {
@@ -20,6 +10,46 @@ namespace Project_Sahlgrenska
         public OrderItems()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_Snus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_Medicin2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_Medicin3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_Equipment(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_Equipment2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_Equipment3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
         }
     }
 }
--- C#/Project_Sahlgrenska/Project_Sahlgrenska/obj/Debug/netcoreapp3.1/App.g.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/obj/Debug/netcoreapp3.1/App.g.cs
@@ -45,7 +45,7 @@ namespace Project_Sahlgrenska {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Project_Sahlgrenska {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public static void Main() {
             Project_Sahlgrenska.App app = new Project_Sahlgrenska.App();
             app.InitializeComponent();
