1d60a1c6125c558e873706215f6e8559d33b306d
Sebastian Lundgren <lundgren_sebastian@hotmail.com>
2021-12-10 09:57:59 +0100

    Prints Available medication and equipment

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/OrderItems.xaml.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/OrderItems.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
+using System.Data;
 using System.Windows;
 
 namespace Project_Sahlgrenska
@@ -22,6 +23,40 @@ namespace Project_Sahlgrenska
         private void AvlEqu_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Hem.conn.Open();
+
+            string query = "SELECT * FROM medication";
+            MySqlCommand cmd = new MySqlCommand(query, Hem.conn);
+            cmd.ExecuteNonQuery();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable("medication");
+            adapter.Fill(dt);
+            AvlMeds.ItemsSource = dt.DefaultView;
+            adapter.Update(dt);
+
+            Hem.conn.Close();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Hem.conn.Open();
+
+            string query = "SELECT * FROM equipment";
+            MySqlCommand cmd = new MySqlCommand(query, Hem.conn);
+            cmd.ExecuteNonQuery();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable("equipment");
+            adapter.Fill(dt);
+            AvlEqu.ItemsSource = dt.DefaultView;
+            adapter.Update(dt);
+
+            Hem.conn.Close();
         }
     }
 }
