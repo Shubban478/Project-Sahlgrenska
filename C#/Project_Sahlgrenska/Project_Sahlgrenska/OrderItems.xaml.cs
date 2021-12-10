8dbbbcda2db1d4b3b82fb5927130398bad7ef961
Sebastian Lundgren <lundgren_sebastian@hotmail.com>
2021-12-10 08:21:55 +0100

    Changed TextBox -> DataGrid

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/OrderItems.xaml.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/OrderItems.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 namespace Project_Sahlgrenska
 {
@@ -7,47 +8,18 @@ namespace Project_Sahlgrenska
     /// </summary>
     public partial class OrderItems : Window
     {
+
         public OrderItems()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void AvlMeds_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
 
         }
 
-        private void CheckBox_Checked_Snus(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void CheckBox_Checked_Medicin2(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void CheckBox_Checked_Medicin3(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void CheckBox_Checked_Equipment(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void CheckBox_Checked_Equipment2(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void CheckBox_Checked_Equipment3(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void AvlEqu_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
 
         }
