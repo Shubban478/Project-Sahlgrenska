7cd8ecd13de1f645e53c07b6495a268f7d326052
Sebastian Lundgren <lundgren_sebastian@hotmail.com>
2021-12-14 19:21:01 +0100

    Room disappears correctly if not vaccant

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/Appointment.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/Appointment.cs
@@ -14,7 +14,6 @@ namespace Project_Sahlgrenska
             Bot.Update("insert into patients_has_rooms values('" + PatientId + "', " + RoomId + ");");
             Bot.Update("update rooms set beds = beds -1 where id =" + RoomId + ";");
             Bot.Update("call update_vaccant_no()");
-            Bot.Update("call update_vaccant_yes()");
             MessageBox.Show(AppointmentId + "\n" +
                             PatientId + "\n" +
                             DoctorId + "\n" +
