d25ea52b64dc7ef8545693552ad50301364c4cd2
Jonathan Limén <jonathan.esbjornsson@gmail.com>
2021-12-15 21:30:23 +0100

    search for symptoms

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/DiseaseList.xaml.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/DiseaseList.xaml.cs
@@ -9,43 +9,67 @@ namespace Project_Sahlgrenska
     /// </summary>
     public partial class DiseaseList : Window
     {
+        private List<string> symptomResult;
+
         public DiseaseList()
         {
             InitializeComponent();
-            PopulateDiseaseList();
+            //PopulateDiseaseList();
         }
 
-        private void PopulateDiseaseList()
+        private void Button_ClickSearch(object sender, RoutedEventArgs e)
         {
+            symptomResult = Bot.ReadOneColumn("SELECT * FROM bt0mlsay6vs1xbceqzzn.diagnosis where Symtoms like '%" + symptomSearch.Text + "%'");
+
             List<string> diseaseList = new List<string>();
 
-            for (int i = 0; i < Bot.ReadOneColumn("select name from diagnosis;").Count; i++)
+            for (int i = 0; i < symptomResult.Count; i++)
             {
-                diseaseList.Add(Bot.ReadOneColumn("select name from diagnosis;")[i]);
+                diseaseList.Add(symptomResult[i]);
             }
-            sjukdom.ItemsSource = diseaseList;
-
+            comboDiseases.ItemsSource = diseaseList;
+            
         }
 
-        private void sjukdom_DropDownClosed(object sender, System.EventArgs e)
+        private void comboDiseases_DropDownClosed(object sender, System.EventArgs e)
         {
-            Hem.conn.Open();
+            diseaseTreatment.Text = Bot.ReadOneValue("SELECT Treatment FROM bt0mlsay6vs1xbceqzzn.diagnosis WHERE Name = '" + comboDiseases.SelectedItem + "';");
 
-            string Query = "select symtoms, treatment from diagnosis where name ='" + sjukdom.Text + "' ";
-
-            MySqlCommand cmd = new MySqlCommand(Query, Hem.conn);
-            MySqlDataReader dr = cmd.ExecuteReader();
-
-            while (dr.Read())
-            {
-                string symtoms = dr.GetString(0);
-                string treatment = dr.GetString(1);
-
-                tsymtom.Text = symtoms;
-                tåtgärd.Text = treatment;
-            }
-
-            Hem.conn.Close();
         }
+
+        //private void PopulateDiseaseList()
+        //{
+        //    List<string> diseaseList = new List<string>();
+
+        //    for (int i = 0; i < Bot.ReadOneColumn("select name from diagnosis;").Count; i++)
+        //    {
+        //        diseaseList.Add(Bot.ReadOneColumn("select name from diagnosis;")[i]);
+        //    }
+        //    sjukdom.ItemsSource = diseaseList;
+
+        //}
+
+        //private void sjukdom_DropDownClosed(object sender, System.EventArgs e)
+        //{
+        //    Hem.conn.Open();
+
+        //    string Query = "select symtoms, treatment from diagnosis where name ='" + sjukdom.Text + "' ";
+
+        //    MySqlCommand cmd = new MySqlCommand(Query, Hem.conn);
+        //    MySqlDataReader dr = cmd.ExecuteReader();
+
+        //    while (dr.Read())
+        //    {
+        //        string symtoms = dr.GetString(0);
+        //        string treatment = dr.GetString(1);
+
+        //        tsymtom.Text = symtoms;
+        //        tåtgärd.Text = treatment;
+        //    }
+
+        //    Hem.conn.Close();
+        //}
+
+
     }
 }
