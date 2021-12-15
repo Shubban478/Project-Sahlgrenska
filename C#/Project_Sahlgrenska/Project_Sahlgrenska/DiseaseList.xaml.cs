d5714be5db82cd74290acaac27e482d78925f975
Jonathan Limén <jonathan.esbjornsson@gmail.com>
2021-12-15 21:54:46 +0100

    cleaned up disease och searchpatient

--- C#/Project_Sahlgrenska/Project_Sahlgrenska/DiseaseList.xaml.cs
+++ C#/Project_Sahlgrenska/Project_Sahlgrenska/DiseaseList.xaml.cs
@@ -14,7 +14,6 @@ namespace Project_Sahlgrenska
         public DiseaseList()
         {
             InitializeComponent();
-            //PopulateDiseaseList();
         }
 
         private void Button_ClickSearch(object sender, RoutedEventArgs e)
@@ -28,48 +27,14 @@ namespace Project_Sahlgrenska
                 diseaseList.Add(symptomResult[i]);
             }
             comboDiseases.ItemsSource = diseaseList;
-            
+            comboDiseases.SelectedIndex = 0;
+            diseaseTreatment.Text = Bot.ReadOneValue("SELECT Treatment FROM bt0mlsay6vs1xbceqzzn.diagnosis WHERE Name = '" + diseaseList[0] + "';");
+
         }
 
         private void comboDiseases_DropDownClosed(object sender, System.EventArgs e)
         {
             diseaseTreatment.Text = Bot.ReadOneValue("SELECT Treatment FROM bt0mlsay6vs1xbceqzzn.diagnosis WHERE Name = '" + comboDiseases.SelectedItem + "';");
-
         }
-
-        //private void PopulateDiseaseList()
-        //{
-        //    List<string> diseaseList = new List<string>();
-
-        //    for (int i = 0; i < Bot.ReadOneColumn("select name from diagnosis;").Count; i++)
-        //    {
-        //        diseaseList.Add(Bot.ReadOneColumn("select name from diagnosis;")[i]);
-        //    }
-        //    sjukdom.ItemsSource = diseaseList;
-
-        //}
-
-        //private void sjukdom_DropDownClosed(object sender, System.EventArgs e)
-        //{
-        //    Hem.conn.Open();
-
-        //    string Query = "select symtoms, treatment from diagnosis where name ='" + sjukdom.Text + "' ";
-
-        //    MySqlCommand cmd = new MySqlCommand(Query, Hem.conn);
-        //    MySqlDataReader dr = cmd.ExecuteReader();
-
-        //    while (dr.Read())
-        //    {
-        //        string symtoms = dr.GetString(0);
-        //        string treatment = dr.GetString(1);
-
-        //        tsymtom.Text = symtoms;
-        //        tåtgärd.Text = treatment;
-        //    }
-
-        //    Hem.conn.Close();
-        //}
-
-
     }
 }
